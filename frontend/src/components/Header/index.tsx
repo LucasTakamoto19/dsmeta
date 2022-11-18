6e8a65b94c807f3c4b1f7a5d0316fa7493c16d37
Lucas Takamoto <lucas.takamoto@hotmail.com>
2022-11-18 12:19:17 -0300

    Other componets

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import './style.css'
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src="#" alt="DSMeta" />
+                    <h1>DSMeta</h1>
+                    <p>Desenvolvido por
+
+                        <a href="https://www.instagram.com/lucastakamoto">@lucastakamoto</a>
+
+                    </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
